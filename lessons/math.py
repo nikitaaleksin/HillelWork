acd9203ca3461bff6d10a6bf6d1b1734158b33b9
Aleksin Nikita <aleksinnikita67@gmail.com>
2026-04-09 01:02:51 +0300

    .idea/workspace.xml

--- lessons/math.py
+++ lessons/math.py
@@ -1,31 +1,18 @@
 a = 50
 b = 25
-print ( a+b)
-
-
-
-int1 = 4
-int2= 2
-float1 = 2.5
-float2 = 2.5
-print(int1 // int2)
-print(float1 / float2)
-
-
-print(3 ** 3)
-
-a = 50
-b = 25
 print ( a+b )
 
-
-
 int1 = 4
 int2= 2
 float1 = 2.5
 float2 = 2.5
 print(int1 // int2)
 print(float1 / float2)
+
+a = 3
+b = 3
+print(a ** b)
+
 
 numbers = [7,9,8,5]
 average = sum(numbers) / len(numbers)
@@ -36,4 +23,17 @@ hours, mins = divmod(minutes, 60)
 print(hours, mins)
 
 prise = 200
+discount_percent = 80
+print ( prise * (discount_percent / 100) )
 
+
+
+
+a = 4
+b = 3
+perimeter = 2 * (a + b)
+print (perimeter)
+
+int = 5490
+last_digit = int % 10
+print ( last_digit )
