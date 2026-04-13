dd9226bc2d42d4b8ebc00b5380d11098cd858d35
Aleksin Nikita <aleksinnikita67@gmail.com>
2026-04-13 17:22:08 +0300

    lessons/bool.py

---
+++ lessons/bool.py
@@ -0,0 +1,16 @@
+# Списки ( остання цифра )
+numbers = [1, 3, 5, 10, 44, 123, 1044]
+numbers.insert(0, 1044)
+numbers.pop(-1)
+print(numbers)
+
+# Список подiл
+things = [ "bread", "butter", "cucumber", "tomato" ]
+print("First list", things)
+things_2 = ["tangerin", "sausage", "coconut"]
+print("Second list:", things_2)
+things.extend(things_2)
+print("Final list", things)
+
+# Калькулятор
+
