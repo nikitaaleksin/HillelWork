bca1b27e6a4f75a5373401e5a94c9bb713f6176b
Aleksin Nikita <aleksinnikita67@gmail.com>
2026-04-20 22:15:56 +0300

    all

---
+++ lessons/lesson_3.py
@@ -0,0 +1,30 @@
+# Нуль на кiнець
+from lessons.math import last_digit
+
+list1 = [123, 0, 45, 0, 67, 0, 234]
+result_with_zero_at_the_end = list()
+count_of_zero_at_the_end = 0
+for num in list1:
+    if num != 0:
+        result_with_zero_at_the_end.append(num)
+    else:
+            count_of_zero_at_the_end += 1
+result_with_zero_at_the_end = result_with_zero_at_the_end + [0] * count_of_zero_at_the_end
+print(result_with_zero_at_the_end)
+
+
+
+
+
+# Множення
+numbers2 = [2, 5, 7, 12, 45, 13, 56]
+sum_lst5 = numbers2[0] + numbers2[2] + numbers2[4]
+print(sum_lst5)
+multiply_lst2 = sum_lst5 * numbers2[-1]
+print(multiply_lst2)
+
+
+
+
+
+
