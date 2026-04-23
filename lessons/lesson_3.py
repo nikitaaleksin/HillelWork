bb193308f38044f3aa3ed4bd32746a08f3de6be7
Aleksin Nikita <aleksinnikita67@gmail.com>
2026-04-23 20:02:38 +0300

    lessons/lesson_3.py

--- lessons/lesson_3.py
+++ lessons/lesson_3.py
@@ -12,15 +12,6 @@ print(result_with_zero_at_the_end)
 
 
 
-
-
-# Множення
-numbers2 = [2, 5, 7, 12, 45, 13, 56]
-sum_lst5 = numbers2[0] + numbers2[2] + numbers2[4]
-print(sum_lst5)
-multiply_lst2 = sum_lst5 * numbers2[-1]
-print(multiply_lst2)
-
 # Три числа
 import random
 
@@ -31,3 +22,30 @@ print("Перший лист", random_list1)
 list2 = [random_list1[0], random_list1[2], random_list1[-2]]
 print("Рузультат", list2)
 
+# Множення
+numbers2 = [int(x) for x in input("Your numbers: ").split(", ")]
+i = 0
+for index, num in enumerate(numbers2):
+    if index % 2 == 0 and index <= 4:
+        i += num
+result = i * numbers2[-1]
+print(result)
+
+
+
+
+
+
+
+
+
+
+
+
+numbers2 = [int(x) for x in input("Your numbers: ").split(", ")]
+i = 0
+for index, num in enumerate(numbers2):
+    if index % 2 == 0 and index <= 4:
+        i += num
+result = i * numbers2[-1]
+print(result)
