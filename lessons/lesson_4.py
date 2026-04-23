03d4b0f5acc43793794163b984a7ac04e96dab64
Aleksin Nikita <aleksinnikita67@gmail.com>
2026-04-23 22:21:56 +0300

    lessons/lesson_4.py

--- lessons/lesson_4.py
+++ lessons/lesson_4.py
@@ -1,28 +1,25 @@
-from operator import truediv  # Модифiкований канкультятор
+# Модифiкований канкультятор
+while True:
+    num1 = float(input("Перше число: "))
+    op = input("Дiя(+, -, *, /,) : ")
+    num2 = float(input("Друге число: "))
+    if op == "+":
+        print(num1 + num2)
+    elif op == "-":
+        print(num1 - num2)
+    elif op == "*":
+        print(num1 * num2)
+    elif op == "/":
+        print(num1 / num2 if num2 != 0 else "Ділення на нуль")
+    else:
+        print("Неправильна дiя")
 
-from prompt_toolkit import choice
-
-#while True:
-  #  num1 = float(input("Перше число: "))
-   # op = input("Дiя(+, -, *, /,) : ")
-   # num2 = float(input("Друге число: "))
-   # if op == "+":
-   #     print(num1 + num2)
-   # elif op == "-":
- #       print(num1 - num2)
-   # elif op == "*":
-      #  print(num1 * num2)
- #   elif op == "/":
-       # print(num1 / num2 if num2 != 0 else "Ділення на нуль")
-  #  else:
-   #     print("Неправильна дiя")
-
-   # choice = input("Продовжити роботу? (y/n):")
-   # if choice == "y":
-    #    continue
-  #  if choice == "n":
-    #    print("Роботу завершено")
-     #   break
+    choice = input("Продовжити роботу? (y/n):")
+    if choice == "y":
+        continue
+    if choice == "n":
+       print("Роботу завершено")
+       break
 
 
 #hashtag
