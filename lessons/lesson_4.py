d2dffe96543140efe0492943a5eeded98e9e08db
Aleksin Nikita <aleksinnikita67@gmail.com>
2026-04-24 12:38:35 +0300

    lesson_4

--- lessons/lesson_4.py
+++ lessons/lesson_4.py
@@ -1,4 +1,6 @@
 #Модифiкований канкультятор
+import keyword
+
 while True:
     num1 = float(input("Перше число: "))
     op = input("Дiя(+, -, *, /,) : ")
@@ -7,6 +9,8 @@ while True:
         print(num1 + num2)
     elif op == "-":
        print(num1 - num2)
+    elif op == "**":
+        print(num1 ** num2)
     elif op == "*":
         print(num1 * num2)
     elif op == "/":
@@ -36,6 +40,29 @@ while True:
     if choice == "n":
      print("Роботу завершено")
      break
+
+
+
+
+
+
+
+from string import digits, punctuation
+user = input('Enter you variable example:')
+is_start_with_digits = user[0].isdigit() if user else False
+prohibited = punctuation.replace('_', '') + ' '
+is_contain_prohibited_symbol = any(s in prohibited for s in user)
+is_double_underscore = '__' in user
+is_contain_uppercase = False
+for s in user:
+    if s.isupper():
+        is_contain_uppercase = True
+        break
+is_keyword = user in keyword.kwlist
+if not is_start_with_digits and not is_contain_uppercase and not is_contain_prohibited_symbol and not is_keyword and not is_double_underscore and user != "":
+    print(True)
+else:
+    print(False)
 
 
 
