79364a7c7e11185501d1146e0043de9c46425e50
Aleksin Nikita <aleksinnikita67@gmail.com>
2026-04-27 19:24:45 +0300

    all

--- Projects/hui.py
+++ Projects/hui.py
@@ -1,35 +1,33 @@
-import turtle
-import math
-turtle.speed(0)
-turtle.color("red")
-turtle.bgcolor("black")
-turtle.pensize(2)
-turtle.setup(width=800, height=800)
-
-def corazon(n):
-    x = 16 * math.sin(n) ** 3
-    y = 13 * math.cos(n) - 5 * math.cos(2 * n) - 2 * math.cos(3 * n) - math.cos(4 * n)
-    return x, y
-for i in range(1, 16):
-    turtle.penup()
+word = input("Your word: ")
+for letter in word:
+    if letter != "a" and letter != "A":
+        print(letter)
 
 
 
-    angle = i * 0.1
 
 
-    for n in range(0, 629, 5):
-        x_raw, y_raw = corazon(n / 100)
+user_number = int(input("Enter a number: "))
+while user_number != 7:
+    print("Попробуй ещё раз: ")
+    user_number = int(input("Enter a number: "))
+if user_number == 7:
+   print("Победа!!")
+
+height = int(input("Enter a height: "))
+width = int(input("Enter a width: "))
+for i in range(height):
+    for j in range(width):
+        print("*", end="")
+    print()
 
 
-        x_scaled = x_raw * i
-        y_scaled = y_raw * i
 
 
-        x = x_scaled * math.cos(angle) - y_scaled * math.sin(angle)
-        y = x_scaled * math.sin(angle) + y_scaled * math.cos(angle)
 
-        turtle.goto(x, y)
-        turtle.pendown()
 
-    turtle.hideturtle()
+
+
+
+
+
