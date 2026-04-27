45e74fa423c7b8e37047c6bab202612561af1728
Aleksin Nikita <aleksinnikita67@gmail.com>
2026-04-27 15:02:30 +0300

    all

--- lessons/lesson_5.py
+++ lessons/lesson_5.py
@@ -17,7 +17,7 @@ while True:
     break
 
 
-#
+#Конвертер із числа в дату
 import time
 user_time = input("Input your number:")
 days, second = divmod(int(user_time), 86400)
@@ -33,5 +33,24 @@ h = str(hours).zfill(2)
 m = str(minutes).zfill(2)
 s = str(seconds).zfill(2)
 print(f"{days}, {day_l}, {h} : {m} : {s}")
+
+
+
+#Добуток чисел
+
+
+user_numbers = input("Input your numbers:")
+while int(user_numbers) > 9:
+    res = 1
+    for digit in str(user_numbers):
+        res *= int(digit)
+    user_numbers = res
+print(f"{user_numbers}")
+
+
+
+
+
+
     
 
