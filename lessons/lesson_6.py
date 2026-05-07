95151115a4318bb3b1f9c7748edc0c914fe3df13
Aleksin Nikita <aleksinnikita67@gmail.com>
2026-05-07 03:19:52 +0300

    all

--- lessons/lesson_6.py
+++ lessons/lesson_6.py
@@ -21,3 +21,13 @@ assert find_unique_value([2, 3, 3, 3, 5, 5]) == 2, 'Test2'
 assert find_unique_value([5, 5, 5, 2, 2, 0.5]) == 0.5, 'Test3'
 print("ОК")
 
+
+
+def is_palindrome(text):
+    sym = "".join(char.lower() for char in text if char.isalnum())
+    return sym == sym[::-1]
+assert is_palindrome('A man, a plan, a canal: Panama') == True, 'Test1'
+assert is_palindrome('0P') == False, 'Test2'
+assert is_palindrome('a.') == True, 'Test3'
+assert is_palindrome('aurora') == False, 'Test4'
+print("OK")
